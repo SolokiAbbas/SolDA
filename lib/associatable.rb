034bc265ca93cee65a3569968c463df690c10d83
Abbas <SolokiAbbas@gmail.com>
2017-09-13 09:19:39 -0400

    removed debugger

--- lib/associatable.rb
+++ lib/associatable.rb
@@ -75,7 +75,6 @@ module Associatable
 
   def has_one_through(name, through_name, source_name)
     define_method(name) do
-      debugger
       through_options = self.class.assoc_options[through_name]
       source_options = through_options.model_class.assoc_options[source_name]
 
